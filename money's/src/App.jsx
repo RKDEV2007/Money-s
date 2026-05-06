52b3a5eba0c9ea91a5e3a3285469b2d2bed7ef0f
RK_DEV <romakarpov90@gmail.com.com>
2026-05-06 10:01:00 +0300

    добавил стартовый неполный экран

--- money's/src/App.jsx
+++ money's/src/App.jsx
@@ -1,10 +1,34 @@
-
+import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Money's - Ваш трекер прибылей и убытков</h1>
-      <h2>тест ветки</h2>
+      <div className="container">
+        <div className="card">
+          <p>Основной баланс</p>
+          <h2>29500 руб.</h2>
+          <p>Доступно для расходов</p>
+        </div>
+        <div className="card">
+          <p>"Подарочный счет"</p>
+          <h2>4000 руб.</h2>
+          <p>10% от дохода</p>
+        </div>
+        <div className="card">
+          <p>Сбережения на "черный день"</p>
+          <h2>4000 руб.</h2>
+          <p>10% от дохода</p>
+        </div>
+        <div className="card">
+          <p>Доходы за месяц</p>
+          <h2>40000 руб.</h2>
+        </div>
+        <div className="card">
+          <p>Расходы за месяц</p>
+          <h2>2500 руб.</h2>
+        </div>
+      </div>
     </div>
   )
 }
